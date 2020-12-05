d85a7c403e89c3d853fcb5bc05b5289c57ccdc26
Martin <martindefs@gmail.com>
2020-12-05 20:38:39 +0100

    Añadida ListView y BD en funcionamiento

--- app/src/main/java/com/example/genshintb/SecondFragment.java
+++ app/src/main/java/com/example/genshintb/SecondFragment.java
@@ -1,15 +1,27 @@
 package com.example.genshintb;
 
+import android.database.Cursor;
 import android.os.Bundle;
+import android.provider.ContactsContract;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.ListFragment;
 import androidx.navigation.fragment.NavHostFragment;
 
-public class SecondFragment extends Fragment {
+import java.util.ArrayList;
+import java.util.List;
+
+public class SecondFragment extends ListFragment {
+
+    ArrayAdapter adapter;
+    List<PersonajeModel> lista = new ArrayList<>();
+    DataAdapter data;
 
     @Override
     public View onCreateView(
@@ -17,7 +29,14 @@ public class SecondFragment extends Fragment {
             Bundle savedInstanceState
     ) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_second, container, false);
+        View view = inflater.inflate(R.layout.fragment_second, container, false);
+
+        data = new DataAdapter(getActivity().getApplicationContext());
+        viewData();
+        adapter = new ArrayAdapter<PersonajeModel>(getActivity(), android.R.layout.simple_list_item_1, lista);
+        setListAdapter(adapter);
+
+        return view;
     }
 
     public void onViewCreated(@NonNull View view, Bundle savedInstanceState) {
@@ -30,5 +49,27 @@ public class SecondFragment extends Fragment {
                         .navigate(R.id.action_SecondFragment_to_FirstFragment);
             }
         });
+    }
+
+    private void viewData(){
+        data.open();
+        Cursor cursor = data.getData();
+        if(cursor.moveToFirst()){
+            do{
+                int personajeID = cursor.getInt(0);
+                String personajeName = cursor.getString(1);
+                int personajeEstrellas = cursor.getInt(2);
+                String personajeElemento = cursor.getString(3);
+                String personajeTipoArma = cursor.getString(4);
+                String personajeImagen = cursor.getString(5);
+
+                PersonajeModel newPersonaje = new PersonajeModel(personajeID, personajeName, personajeEstrellas, personajeElemento,
+                        personajeTipoArma, personajeImagen);
+
+                lista.add(newPersonaje);
+
+            }while(cursor.moveToNext());
+        }
+        data.close();
     }
 }
